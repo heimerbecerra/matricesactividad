ccfa4a33505ad2d8bbe255e62aa75c9332d8125d
heimerbecerra <158522034+heimerbecerra@users.noreply.github.com>
2024-05-23 12:42:04 -0500

    Add files via upload

---
+++ matrices1.py
@@ -0,0 +1,141 @@
+matriz = [['-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-'], ['-', '-', '-', '-', '-', '-']]
+dato = print('Ingrese dato: ')
+
+
+def fnt_agregar(dato, x, y):
+    if dato == 'A' or dato == 'a' and (x == 0 and y == 0):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>')
+    elif dato == 'B' or dato == 'b' and (x == 0 and y == 1):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>')
+    elif dato == 'C' or dato == 'c' and (x == 0 and y == 2):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>')
+    elif dato == 'D' or dato == 'd' and (x == 0 and y == 3):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>')
+    elif dato == 'E' or dato == 'e' and (x == 0 and y == 4):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>')
+    elif dato == 'F' or dato == 'f' and (x == 1 and y == 0):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>')
+    elif dato == 'G' or dato == 'g' and (x == 1 and y == 1):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>')       
+    elif dato == 'H' or dato == 'h' and (x == 1 and y == 2):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>')
+    elif dato == 'I' or dato == 'i' and (x == 1 and y == 3):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>')
+    elif dato == 'J' or dato == 'j' and (x == 1 and y == 4):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>')
+    elif dato == 'K' or dato == 'k' and (x == 2 and y == 0):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>')
+    elif dato == 'L' or dato == 'l' and (x == 2 and y == 1):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'M' or dato == 'm' and (x == 2 and y == 2):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'N' or dato == 'n' and (x == 2 and y == 3):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'Ñ' or dato == 'ñ' and (x == 2 and y == 4):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'O' or dato == 'o' and (x == 3 and y == 0):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'P' or dato == 'p' and (x == 3 and y == 1):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'Q' or dato == 'q' and (x == 3 and y == 2):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'R' or dato == 'r' and (x == 3 and y == 3):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'S' or dato == 's' and (x == 3 and y == 4):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'T' or dato == 't' and (x == 4 and y == 0):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'U' or dato == 'u' and (x == 4 and y == 1):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'V' or dato == 'v' and (x == 4 and y == 2):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'W' or dato == 'w' and (x == 4 and y == 3):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'X' or dato == 'x' and (x == 4 and y == 4):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'Y' or dato == 'y' and (x == 5 and y == 0):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'Z' or dato == 'z' and (x == 5 and y == 1):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'CH' or dato == 'ch' and (x == 5 and y == 2):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'LL' or dato == 'll' and (x == 5 and y == 3):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    elif dato == 'RR' or dato == 'rr' and (x == 5 and y == 4):
+        if matriz [x][y] == '-':
+            matriz [x][y] = dato.upper()
+            input('Posición correcta <ENTER>') 
+    else:
+        input('Posición Incorrecta <ENTER>')
+        
+def fnt_impresion_matriz():
+    for i in range(len(matriz)):
+        for j in range(len(matriz[i])):
+            print(matriz[i][j], end='  ')
+        print()
+
+sw = True
+
+while sw == True:
+    import os
+    os.system('cls')
+    fnt_impresion_matriz()
+    fnt_agregar(input('Dato: '),int(input('Fila: ')), int(input('Columna: ')))
